acfaef49f50cb399c34f69110fa0576530cb3722
JT Reyes <jesustgreyes@gmail.com>
2023-09-28 12:15:15 -0700

    set up main.jsx

--- react-portfolio/src/main.jsx
+++ react-portfolio/src/main.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from './App'
+
+import './assets/index.css'  // CSS FILE
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import AboutMe from './pages/AboutMe';
+import Projects from './pages/Projects';    
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+
+const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <App />,
+      children: [
+        {
+          index: true,
+          element: <AboutMe/>,
+        },
+        {
+          path: '/Projects',
+          element: <Projects/>,
+        },
+        {
+          path: '/Contact',
+          element: <Contact />
+        },
+        {
+          path: '/Resume',
+          element: <Resume />
+        }
+      ],
+    },
+  ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  );
